0213ea576880d51de9243249f3e559a11f52f274
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-03 01:04:32 +0600

    show temp commit

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const api_key = `63718458320aec7594636e977d2f6544`;
 
 const setTemperature = () =>{
     const city =document.getElementById('city-name').value;
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key }`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key }&units=metric`;
     console.log(url);
     fetch(url)
     .then(res =>res.json())
     .then(data => displayTemperature(data))
 }
-
+const setTemperatureText = (id,text)=>{
+     document.getElementById(id).innerText=text;
+}
 const displayTemperature = temperature =>{
+    setTemperatureText('city',temperature.name);
+    setTemperatureText('temperature',temperature.main.temp);
     console.log(temperature );
 }
